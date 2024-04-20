5bf06035bfb1bbf3a88eb8b1c11226b542b91cec
Marcvs99 <101611258+Marcvs99@users.noreply.github.com>
2024-04-20 01:48:41 -0400

    Update deployment.sh

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -3,13 +3,11 @@
 # Define variables
 IMAGE_NAME="marcvs99/ceg3140_project4"
 TAG="latest"
-CONTAINER_NAME=$HOSTNAME
 
+docker stop $(docker ps -q)
 
-docker stop $CONTAINER_NAME
-
-docker rm $CONTAINER_NAME
+docker rm -f $(docker ps -a -q)
 
 docker pull $IMAGE_NAME:$TAG
 
-docker run -d -p 80:80 --name $IMAGE_NAME:$TAG
+docker run -d -p 80:80 $IMAGE_NAME:$TAG
